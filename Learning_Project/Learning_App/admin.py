85d7c146df5c389c6f21514e23cb948efe991c72
saumyasaxenaa <saumya.mins@gmail.com>
2020-07-29 16:55:16 -0700

    Project

--- Learning_Project/Learning_App/admin.py
+++ Learning_Project/Learning_App/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Attributes
+
+admin.site.register(Attributes)
--- Learning_Project/Learning_App/models.py
+++ Learning_Project/Learning_App/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Attributes(models.Model):
+    Name = models.CharField(max_length=50)
+    ID = models.AutoField(primary_key=True)
+    Contact = models.IntegerField(null=False, blank=False, unique=True)
+    Address = models.CharField(max_length=100)
